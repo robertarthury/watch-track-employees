278e9a5f5b96502df3c4286a24c8ee092b5a7820
robertarthury <robertarthury@gmail.com>
2022-06-13 23:25:22 -0400

    one day i will figure this out

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 // connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: process.env.MYSQL_PASSWORD,
   database: 'employee_db'
